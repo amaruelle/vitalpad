2b4b2c0e20ee0a2e494ad8f7bb4d02c1ab50a320
AmaruelleOF <amaruelleof@gmail.com>
2023-02-19 18:58:01 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.InteropServices; // For DllImport
+using WinRT;
+using System.Xml.Linq;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MicroexplorerApp
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See below for implementation.
+        MicaController m_backdropController;
+        SystemBackdropConfiguration m_configurationSource;
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            TrySetSystemBackdrop();
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(AppTitleBar);
+        }
+
+        
+
+        private void Menu_Opening(object sender, object e)
+        {
+            CommandBarFlyout myFlyout = sender as CommandBarFlyout;
+            if (myFlyout.Target == REBCustom)
+            {
+                AppBarButton myButton = new AppBarButton();
+                myButton.Command = new StandardUICommand(StandardUICommandKind.Share);
+                myFlyout.PrimaryCommands.Add(myButton);
+            }
+        }
+
+        private void REBCustom_Loaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening += Menu_Opening;
+            REBCustom.ContextFlyout.Opening += Menu_Opening;
+        }
+
+        private void REBCustom_Unloaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening -= Menu_Opening;
+            REBCustom.ContextFlyout.Opening -= Menu_Opening;
+        }
+
+        private void REBCustom_TextChanged(object sender, RoutedEventArgs e)
+        {
+            string value = string.Empty;
+            REBCustom.Document.GetText(Microsoft.UI.Text.TextGetOptions.AdjustCrlf, out value);
+            symbolsCount.Text = "Symbols count: " + value.Length;
+            char[] delimiters = new char[] { ' ', ',', '.', ';', ':', '?', '!' };
+            string[] words = value.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            wordsCount.Text = "Words count: " + words.Length;
+        }
+
+        bool TrySetSystemBackdrop()
+        {
+            if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+            {
+                m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+                m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+                // Create the policy object.
+                m_configurationSource = new SystemBackdropConfiguration();
+                this.Activated += Window_Activated;
+                this.Closed += Window_Closed;
+                ((FrameworkElement)this.Content).ActualThemeChanged += Window_ThemeChanged;
+
+                // Initial configuration state.
+                m_configurationSource.IsInputActive = true;
+                SetConfigurationSourceTheme();
+
+                m_backdropController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+    
+            // Enable the system backdrop.
+            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+            m_backdropController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                m_backdropController.SetSystemBackdropConfiguration(m_configurationSource);
+                return true; // succeeded
+            }
+
+            return false; // Mica is not supported on this system
+        }
+
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            // Make sure any Mica/Acrylic controller is disposed
+            // so it doesn't try to use this closed window.
+            if (m_backdropController != null)
+            {
+                m_backdropController.Dispose();
+                m_backdropController = null;
+            }
+            this.Activated -= Window_Activated;
+            m_configurationSource = null;
+        }
+
+        private void Window_ThemeChanged(FrameworkElement sender, object args)
+        {
+            if (m_configurationSource != null)
+            {
+                SetConfigurationSourceTheme();
+            }
+        }
+
+        private void SetConfigurationSourceTheme()
+        {
+            switch (((FrameworkElement)this.Content).ActualTheme)
+            {
+                case ElementTheme.Dark: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+                case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+                case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+            }
+        }
+    }
+
+    class WindowsSystemDispatcherQueueHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct DispatcherQueueOptions
+        {
+            internal int dwSize;
+            internal int threadType;
+            internal int apartmentType;
+        }
+
+        [DllImport("CoreMessaging.dll")]
+        private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+        object m_dispatcherQueueController = null;
+        public void EnsureWindowsSystemDispatcherQueueController()
+        {
+            if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+            {
+                // one already exists, so we'll just use it.
+                return;
+            }
+
+            if (m_dispatcherQueueController == null)
+            {
+                DispatcherQueueOptions options;
+                options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+                options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+                options.apartmentType = 2; // DQTAT_COM_STA
+
+                CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+            }
+        }
+    }
+}
